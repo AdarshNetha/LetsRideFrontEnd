219cb2c736286755cbefada1e99ee47ea8485549
vamshi reddy <vamshireddy0228@gmail.com>
2026-01-09 18:21:15 +0530

    added booking history

--- driver-booking-history.js
+++ driver-booking-history.js
@@ -1,37 +1,50 @@
 const mobileNo = localStorage.getItem("mobileNo");
-
+const token = localStorage.getItem("token");
 function goBack() {
     window.history.back();
 }
 
-fetch(`http://localhost:8085/driver/booking-history?mobileno=${mobileNo}`)
+if (!mobileNo) {
+    alert("Mobile number not found. Please login again.");
+}
+
+fetch(`http://localhost:8085/driver/booking-history?mobileno=${mobileNo}`, {
+    method: "GET",
+            headers: {
+        "Authorization": `${token}`,
+        "Content-Type": "application/json"
+    }
+    
+        })
     .then(res => res.json())
     .then(result => {
-            
+
         const tbody = document.getElementById("historyBody");
         const totalFareEl = document.getElementById("totalFare");
 
         tbody.innerHTML = "";
 
-        if (!result.data || result.data.rideDTOs.length === 0) {
+        const history = result.data.history;
+        const totalAmount = result.data.totalAmount;
+
+        if (!history || history.length === 0) {
             tbody.innerHTML = `
                 <tr>
-                    <td colspan="5" class="loading">No rides found</td>
+                    <td colspan="5">No rides found</td>
                 </tr>`;
             return;
         }
 
-        // Total earnings
-        totalFareEl.innerText = `₹${result.data.total}`;
+        totalFareEl.innerText = `₹${totalAmount.toFixed(2)}`;
 
-        result.data.rideDTOs.forEach(r => {
+        history.forEach(r => {
             tbody.innerHTML += `
                 <tr>
                     <td>${r.bookingId}</td>
                     <td>${r.fromLocation}</td>
                     <td>${r.toLocation}</td>
                     <td>${r.distance.toFixed(2)}</td>
-                    <td>₹${r.fare}</td>
+                    <td>₹${r.fare.toFixed(2)}</td>
                 </tr>`;
         });
     })
@@ -39,4 +52,3 @@ fetch(`http://localhost:8085/driver/booking-history?mobileno=${mobileNo}`)
         console.error(err);
         alert("Failed to load booking history");
     });
-    
